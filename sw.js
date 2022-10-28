625ee5633689d224fd746be284d3e9e831bc4e8e
Wilberth <wilberthlp4@gmail.com>
2022-10-28 16:15:21 -0500

    Creación de manifest

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const STATIC_CACHE = 'static-v1';
+// imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -13,6 +16,7 @@ const APP_SHELL = [
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
     'js/app.js',
+    'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -31,7 +35,7 @@ self.addEventListener('install', e => {
     const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => {
         cache.addAll(APP_SHELL_INMUTABLE);
     })
-    e.waitUntil(Promiso.all([cacheStatic, cacheInmutable]));
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 })
 
 self.addEventListener('activate', e=>{
@@ -43,4 +47,18 @@ self.addEventListener('activate', e=>{
         })
     })
     e.waitUntil(respuesta);
+});
+
+self.addEventListener('fetch', e => {
+    const respuesta = caches.match(e.request).then(resp => {
+        if(resp){
+            return resp;
+        }else{
+            // console.log(e.request.url);
+            return fetch(e.request).then(newResp => {
+                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newResp);
+            })
+        }
+    })
+    e.respondWith(respuesta);
 })
