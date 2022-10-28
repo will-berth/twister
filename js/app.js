5418c5c462a77a5a182a3571430e49eb0d5b1999
Wilberth <wilberthlp4@gmail.com>
2022-10-28 16:18:26 -0500

    Modificación de ruta de obtención del serviceworker (local, producción)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+let url = windows.location.href;
+let swLocation = '/twister/sw.js';
+
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+    if(url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation);
 }
 // Referencias de jQuery
 
